90b7ff12805353c799985f7d4f766cf67222d048
Abdullah Ayhan <aayhaan4@gmail.com>
2021-11-25 17:47:47 +0300

    Güncelleme

--- VizeWeb/Controllers/HomeController.cs
+++ VizeWeb/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using VizeWeb.DatabaseContext2;
 using VizeWeb.Models;
 
@@ -37,6 +39,34 @@ namespace VizeWeb.Controllers
             return View(Uyeler);
         }
 
+        public IActionResult BestTeams()
+        {
+            Random random = new Random();
+            List<Takim> takimlar= new List<Takim>();
+            int totalTakimSayisi=databaseContext.Takimlar.Count();
+            for (int i = 0; i < 10; i++)
+            {
+                var randomTakim=databaseContext.Takimlar.Find(random.Next(0, totalTakimSayisi));
+                if (!takimlar.Contains(randomTakim))
+                {
+                    takimlar.Add(randomTakim);
+                }
+            }
+            return View();
+        }
+
+        public string NonTeamMembers()
+        {
+            List<Uye> NonTeamMembers = (databaseContext.Uyeler.Where(x=>x.TakimID==null)).ToList();
+            string x = JsonConvert.SerializeObject(NonTeamMembers);
+            return x;
+        }
+
+        public IActionResult GetAllTeam(Takim takim)
+        {
+            List<Takim> Takimlar = databaseContext.Takimlar.ToList();
+            return View(Takimlar);
+        }
 
     }
 }
