e2bb10839ccaca32a5b0575dece08b19042e74da
Abdullah Ayhan <aayhaan4@gmail.com>
2021-11-25 16:43:09 +0300

    ss

--- VizeWeb/Controllers/HomeController.cs
+++ VizeWeb/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -35,5 +34,7 @@ namespace VizeWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
