2b8cacb5e7b21af66e284bc3316c5ba54db56964
Abdullah Ayhan <aayhaan4@gmail.com>
2021-11-26 18:54:20 +0300

    son son

--- VizeWeb/DbContext/DatabaseContext.cs
+++ VizeWeb/DbContext/DatabaseContext.cs
@@ -10,7 +10,7 @@ namespace VizeWeb.DatabaseContext2
 {
     public class DatabaseContext : DbContext
     {
-        string connectionString = DBSettings.Ins.SQLServer;
+        string connectionString = "Data Source=.;Initial Catalog=TeknoFestOdev;Integrated Security=True"/*DBSettings.Ins.SQLServer*/;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString);
