47ec6e58659b24725dcfe020588eee48c239ae65
Abdullah Ayhan <aayhaan4@gmail.com>
2021-12-01 13:51:31 +0300

    duyurular

---
+++ VizeWeb/Controllers/Announcement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VizeWeb.business;
+using VizeWeb.DatabaseContext2;
+
+namespace VizeWeb.Controllers
+{
+    public class Announcement : Controller
+    {
+        public IActionResult Index()
+        {
+            DatabaseContext databaseContext = new DatabaseContext();
+            var model = databaseContext.GetAllAnnouncement();
+            return View(model);
+        }
+
+    }
+}
